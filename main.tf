00c912df37c18abaf6bd69a905ab8be2be35523c
alexandarp <alexandar.podobnik@gmail.com>
2019-03-21 11:52:52 -0400

    add credentials to provider

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ terraform {
 provider "aws" {
   # TODO change to a variable in CI
   #shared_credentials_file = "/home/alexandar/.aws/credentials"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region = "us-east-1"
 }
 
